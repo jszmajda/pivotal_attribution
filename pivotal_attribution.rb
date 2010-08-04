993f14c2d0278a4c85ad7c9df54ccfc25d472e6a
Joshua Szmajda <josh@optoro.com>
2010-08-04 12:12:53 -0400

    added date headings, not showing future iterations, added permanent faker option

--- pivotal_attribution.rb
+++ pivotal_attribution.rb
@@ -5,6 +5,24 @@ require 'fastercsv'
 require 'chronic'
 require 'active_support'
 
+if ARGV.length < 1
+  usage =<<-EOT
+Usage: #{__FILE__} <Your CSV File Here> <Optional Report Start Date>
+
+Example:
+  #{__FILE__} my_awesome_project_20100803_1919.csv "Two Weeks Ago"
+
+  EOT
+  puts usage
+  exit
+end
+
+use_faker = false
+if ARGV[2] && ARGV[2] == "faker"
+  require 'faker'
+  use_faker = true
+end
+
 def col_num(val, pad, inverse=false)
   v = val.to_i
   s = case v
@@ -30,8 +48,10 @@ since = Chronic.parse(ARGV[1]) if ARGV[1]
 puts "\e[1;32mSince #{since || "Project Inception"}\e[0m"
 
 iterations = []
+idates = []
 current_iteration = nil
 iteration = {}
+faker_map = {}
 FasterCSV.foreach(ARGV.first, :headers => true) do |row|
   #<FasterCSV::Row 
   #  "Id"              : "3090942"
@@ -65,6 +85,10 @@ FasterCSV.foreach(ARGV.first, :headers => true) do |row|
   since = Time.parse(row["Created at"]) if since.nil?
 
   u = row["Owned By"]
+  if use_faker
+    faker_map[u] = Faker::Name.name unless faker_map[u]
+    u = faker_map[u]
+  end
   u = "Unassigned" if u.nil?
   user_stories[u] = {:before => {:complete => [], :incomplete => []}, :after => {:complete => [], :incomplete => []}} if user_stories[u].nil?
 
@@ -75,10 +99,16 @@ FasterCSV.foreach(ARGV.first, :headers => true) do |row|
 
   user_stories[u][whenly][complete] << row if row["Story Type"] == "feature"
 
+  iend = row["Iteration End"]
+  next if iend.nil? || Time.parse(iend) > Time.now
+
   itr = row["Iteration"]
   if current_iteration != itr
     current_iteration = itr
-    iterations << iteration if iteration.keys.size > 0
+    if iteration.keys.size > 0
+      iterations << iteration 
+      idates << Time.parse(iend)
+    end
     iteration = {}
   end
   iteration[u] = 0 if iteration[u].nil?
@@ -99,7 +129,7 @@ end
 
 puts "\n\e[1;32m#{iterations.size} Iterations\e[0m"
 
-#puts "#{"".ljust(max_username.length)} #{iterations.collect{|itr| "".ljust(2)}.join(" | ")}"
+puts "#{"".ljust(max_username.length)} | #{idates.collect{|itr| "\e[33m"+itr.strftime("%m/%d").ljust(5) +"\e[0m"}.join(" | ")} |"
 users.each do |user|
-  puts "#{user_color(user,max_username.length)} | #{iterations.collect{|itr| col_num(itr[user], 2)}.join(" | ")} |"
+  puts "#{user_color(user,max_username.length)} | #{iterations.collect{|itr| col_num(itr[user], 5)}.join(" | ")} |"
 end
